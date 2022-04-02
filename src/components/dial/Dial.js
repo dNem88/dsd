f93ca85caeb9d5065c6d855f4519df561ec9cb32
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-04-02 13:58:51 +0300

    phone icon

--- src/components/dial/Dial.js
+++ src/components/dial/Dial.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './Dial.module.css'
+import phone from '../../assets/phone.svg'
 
 function Dial({phoneNumber}) {
 
     return (
         <div className={styles.container}>
-            <a href={`tel:${phoneNumber || ''}`}>Dial</a>
+            <a href={`tel:${phoneNumber || ''}`}>
+                <img src={phone}/>
+            </a>
         </div>
     )
 }
