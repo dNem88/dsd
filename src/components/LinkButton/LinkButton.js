ab85500f0799a499730008ea25a1caf0c52664cd
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-04-02 13:13:26 +0300

    dial

--- src/components/LinkButton/LinkButton.js
+++ src/components/LinkButton/LinkButton.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import styles from './LinkButton.module.css' 
+import add from '../../assets/add.svg'
 
-function LinkButton({href,content, className, clickHandler=() => null}) {
+function LinkButton({href,content, image, clickHandler=() => null}) {
 
     return (
-        <Link onClick={clickHandler} className={styles[className]} to={href} styles={styles}>{content}</Link>
+        <div className={styles.container} onClick={clickHandler}>
+            <div className={styles['image-container']}>
+                <img src={add}/>
+            </div>
+            <Link  to={''} styles={styles}>{content}</Link>
+        </div>
     )
 }
 export default LinkButton
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,13 +4,15 @@ import LinkButton from '../LinkButton/LinkButton'
 import Offer from './Offer'
 import Spinner from '../spinner/Spinner'
 import ErrorComp from '../errorComp/ErrorComp'
+import {useNavigate} from 'react-router-dom'
 
 function HomePage({offers}) {
     console.log(offers)
-
+    const navigate = useNavigate()
+    
     return (
         <div className={styles.container}>
-            <LinkButton href={'/offers/add'} content={'добави'} className={'blue'}/>
+            <LinkButton href={'/offers/add'} content={'добави'} className={'blue'} clickHandler={() => {navigate('/offers/add')}}/>
             {!offers.error ? offers ? offers.offers.map((x,i) => {
                 return <Offer key={i} _id={x._id} hood={x.hood || 'N/A'} price={x.price || 'N/A'} address={x.address || 'N/A'}/>
             }) : <Spinner/> : <ErrorComp errorMessage={'Failed to fetch'}/>}
